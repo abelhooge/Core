aab34844eee4829a070cc7a9e84fe43c4a70153b
Abel Hoogeveen <abel@techfuze.net>
2015-05-06 21:25:57 +0200

    Implemented renewed querybuilder and a better databasemodel. Database utilities can from now on be added to this module. Fixes #46

--- Core/System/Models/model.interpret.php
+++ Core/System/Models/model.interpret.php
@@ -7,21 +7,13 @@ class Interpret extends Model {
     public function __construct(&$core){
         parent::__construct($core);
 
-        $this->setType('techfuze/databasemodel', 'DatabaseModel');
-        $this->fields   = array();
+        $this->setType('techfuze/databaseutils', 'Model');
         $this->table    = '';
     }
 
     public function table($name) {
     	$this->table = $name;
 
-    	// Get tables
-    	$sth = $this->mods->database->prepare("DESCRIBE " . $this->table);
-    	$sth->execute();
-		$table_fields = $sth->fetchAll(\PDO::FETCH_COLUMN);
-
-		// Append to model
-		$this->fields = $table_fields;
     }
 }
 
---
+++ Modules/databaseutils/moduleInfo.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+
+    'module_class'    => 'Module\DatabaseUtils\Main',
+    'module_file'     => 'class.model.php',
+    'module_name'     => 'DatabaseUtils',
+
+    'abstract'      => false,
+    'dependencies'  => array('techfuze/database'),
+    'events'        => array(),
+    'sections'      => array(),
+
+    'name'          => 'FuzeWorks Database Utilities',
+    'description'   => 'Automatically build SQL queries using methods in this class',
+    'author'        => 'TechFuze',
+    'version'       => '1.0.0',
+    'website'       => 'http://fuzeworks.techfuze.net/',
+
+    'date_created'  => '29-04-2015',
+    'date_updated'  => '29-04-2015',
+
+    'enabled'       => true,
+);
